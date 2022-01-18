077b197d75377ac61196e1e11779836664d9bcf9
crrojas88 <carlorrojas@gmail.com>
2022-01-18 14:41:31 -0800

    first commit of nasagram

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [apod, setApod] = useState([])
+
+  // let url = 'https://api.nasa.gov/planetary/apod?api_key=0DcEwhhQj1GqafMMwf1RKiROabpnhQh8YTfhZCTx'
+
+  const hook = () => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=0DcEwhhQj1GqafMMwf1RKiROabpnhQh8YTfhZCTx')
+    .then(response => {setApod(response.data)})
+  }
+  useEffect(hook, [])
+
+  console.log(apod)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={apod.url}/>
     </div>
   );
 }
